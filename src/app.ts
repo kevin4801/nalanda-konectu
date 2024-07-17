19f207ed659ac471f890948042c9ebb20bec0e5a
dipeshtumbada <134152238+dipeshtumbada@users.noreply.github.com>
2024-07-18 02:07:55 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,20 +11,25 @@ const port = getPort();
 const app = express();
 const server = http.createServer(app);
 
+// CORS configuration
 const corsOptions = {
-  origin: '*',
+  origin: 'http://34.207.105.195', // Replace with your frontend URL or '*'
 };
 
-app.use(cors(corsOptions));
-app.use(express.json());
+// Middleware
+app.use(cors(corsOptions)); // Enable CORS with specified options
+app.use(express.json()); // Parse JSON request bodies
 
-app.use('/api', routes);
+// Routes
+app.use('/api', routes); // Mount API routes
 app.get('/api', (_, res) => {
-  res.status(200).json({ status: 'ok' });
+  res.status(200).json({ status: 'ok' }); // Example endpoint response
 });
 
+// Start server
 server.listen(port, () => {
   logger.info(`Server is running on port ${port}`);
 });
 
+// Start WebSocket server
 startWebSocketServer(server);
