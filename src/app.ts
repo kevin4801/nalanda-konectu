392b09abca41e3a45db980996ab5e1fc57e4dc08
dipeshtumbada <134152238+dipeshtumbada@users.noreply.github.com>
2024-07-18 22:33:27 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { startWebSocketServer } from './websocket';
 import express from 'express';
-import cors from 'cors';
+import cors from 'cors'; // Import cors middleware
 import http from 'http';
 import routes from './routes';
 import { getPort } from './config';
@@ -11,14 +11,17 @@ const port = getPort();
 const app = express();
 const server = http.createServer(app);
 
+// Define CORS options to allow requests from your frontend
 const corsOptions = {
-  origin: '*',
+  origin: 'http://34.207.105.195', // Replace with your frontend URL
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); // Apply CORS middleware
 app.use(express.json());
 
 app.use('/api', routes);
+
 app.get('/api', (_, res) => {
   res.status(200).json({ status: 'ok' });
 });
